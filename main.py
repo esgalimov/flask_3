a389b741d7161157cc675e13180d6befe778a148
esgalimov <esgalimov@gmail.com>
2021-03-08 19:04:58 +0300

    WEB. Знакомство с flask-sqlalchemy. Задача 4. Первая работа. main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask
 from data import db_session
 from data.users import User
 from data.jobs import Jobs
+import datetime
 
 
 app = Flask(__name__)
@@ -10,16 +11,15 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 def main():
     db_session.global_init("db/blogs.db")
-    user = User()
-    user.surname = 'Scott'
-    user.name = 'Ridley'
-    user.age = 21
-    user.position = 'captain'
-    user.speciality = 'research engineer'
-    user.address = 'module_1'
-    user.email = 'scott_chief@mars.org'
+    job = Jobs()
+    job.team_leader = 1
+    job.job = 'deployment of residential modules 1 and 2'
+    job.work_size = 15
+    job.collaborators = '2, 3'
+    job.start_date = datetime.datetime.now()
+    job.is_finished = False
     db_sess = db_session.create_session()
-    db_sess.add(user)
+    db_sess.add(job)
     db_sess.commit()
     # app.run()
 
