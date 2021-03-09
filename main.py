0cda2d6d61589123dad00eed1af25eb96cc2d55f
esgalimov <esgalimov@gmail.com>
2021-03-09 18:29:11 +0300

    WEB. Знакомство с flask-sqlalchemy. Задача 13. Форма регистрации. main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask import redirect
 from data import db_session
 from data.users import User
 from flask import render_template
 from data.jobs import Jobs
 import datetime
+from forms.user import RegisterForm
 
 
 app = Flask(__name__)
@@ -14,8 +16,37 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 def job_list():
     db_sess = db_session.create_session()
     jobs = db_sess.query(Jobs)
-    print(jobs[0].team_leader)
     return render_template('index.html', jobs=jobs, title='Журнал работ')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/login')
+def login():
+    return 'success'
 
 
 def main():
