13b67d755f6cd518017f32b4f0fb77f059029e51
esgalimov <esgalimov@gmail.com>
2021-03-12 18:01:34 +0300

    WEB. Flask-sqlalchemy. Задача 1. Модель формы авторизации пользователя. login_form.py, main.py

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from flask import Flask
-from flask import redirect
 from data import db_session
 from data.users import User
-from flask import render_template
+from flask import render_template, make_response, request, redirect
 from data.jobs import Jobs
 import datetime
 from forms.user import RegisterForm
+from forms.login_form import LoginForm
+from flask_login import LoginManager, login_user, logout_user, login_required
 
 
 app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
@@ -52,6 +55,34 @@ def reqister():
 @app.route('/login')
 def login():
     return 'success'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
 
 
 def main():
